0c14397b87f7ee15098d7bcbedf8efd586e80bd5
NTKhang <81167726+ntkhang03@users.noreply.github.com>
2021-07-25 11:04:52 +0700

    Update notifications.js

--- notifications.js
+++ notifications.js
@@ -1,1 +1,1 @@
-["Hi","file này sau khi tải về có thể xóa đi nhé"]
+["Đã fix lỗi say, truy cập vào https://github.com/ntkhang03/mirai-features/blob/features/modules/commands/say.js sau đó copy code paste vào modules/commands/say.js","file này sau khi tải về có thể xóa đi nhé"]
